5340205dedf51a47dbf11df422b7c19eb57972f7
justin <justinlatta6@gmail.com>
2022-12-09 23:24:02 -0700

    Init commit

---
+++ src/main/java/net/northerncrm/northerncrm/NorthernCrmApplication.java
@@ -0,0 +1,13 @@
+package net.northerncrm.northerncrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthernCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorthernCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/northerncrm/northerncrm/NorthernCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package net.northerncrm.northerncrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthernCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
